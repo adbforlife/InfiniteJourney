5e670345676241d2d4dda2ed3b55783ff408ed17
Albert Gao <adbforlife2018@gmail.com>
2017-05-16 08:49:49 -0400

    Fix coin

--- InfiniteJourney/Levels.swift
+++ InfiniteJourney/Levels.swift
@@ -11,8 +11,7 @@ import SpriteKit
 import GameplayKit
 
 class Levels: SKScene {
-    var levelDistances: [Int]=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
-    var levelMessages: [String]=["I am the One. For now, move on to Level 2.", "Have you ever wondered who you are?", "You're getting good. Level 10 will be your doom.", "Level 4? My matrix must recailbrate to deal with your skill."]
+    var levelMessages: [String]=["I am the One. For now, move on to Level 2.", "Have you ever wondered who you are?", "You're getting good. Level 10 will be your doom.", "Level 4? My matrix must recalibrate to deal with your skill."]
 }
 
 
